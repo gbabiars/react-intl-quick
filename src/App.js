cd5753e6dbd8ec6aa8428df1a82772e1a866db70
Greg Babiars <gbabiars@gmail.com>
2016-12-14 18:23:28 -0700

    Initial project

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import {
+  IntlProvider,
+  addLocaleData,
+  defineMessages,
+  FormattedMessage,
+  FormattedNumber,
+  FormattedDate,
+  FormattedTime
+} from 'react-intl';
+import locales from './locales';
+
+const contentMessages = defineMessages({
+  count: {
+    id: 'body.count',
+    defaultMessage: `Look, I can pluralize {count, plural,
+      one {{count} number}
+      other {{count} new contacts}
+    }`
+  },
+  date: {
+    id: 'body.date',
+    defaultMessage: `Today is {date}`
+  },
+  time: {
+    id: 'body.time',
+    defaultMessage: `The time is {time}`
+  }
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locale: null,
+      formats: null,
+      messages: null,
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    locales(navigator.language, ({locale, formats, messages}) => {
+      this.setState({
+        locale,
+        formats,
+        messages,
+        isLoaded: true
+      });
+    });
+  }
+
+  render() {
+    const {locale, formats, messages, isLoaded} = this.state;
+
+    if(!isLoaded) {
+      return <div>Loading...</div>
+    }
+
+    if(formats) {
+      addLocaleData(formats);
+    }
+
+    return (
+      <IntlProvider locale={locale} messages={messages}>
+        <div>
+          <h3>
+            <FormattedMessage id="header.welcome" defaultMessage="Welcome" />
+          </h3>
+          <div>
+            <p>
+              <FormattedMessage {...contentMessages.count}
+                                values={{
+                                  count: <FormattedNumber value={1}/>
+                                }} />
+            </p>
+            <p>
+              <FormattedMessage {...contentMessages.count}
+                                values={{
+                                  count: <FormattedNumber value={2000}/>
+                                }} />
+            </p>
+            <p>
+              <FormattedMessage {...contentMessages.date}
+                                values={{
+                                  date: <FormattedDate value={new Date()} />
+                                }} />
+            </p>
+            <p>
+              <FormattedMessage {...contentMessages.time}
+                                values={{
+                                  time: <FormattedTime value={new Date()} />
+                                }} />
+            </p>
+          </div>
+        </div>
+      </IntlProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/locales/index.js
@@ -0,0 +1,101 @@
+const hasIntl = typeof(Intl) !== 'undefined';
+
+function en(cb) {
+  if(!hasIntl) {
+    require.ensure(['./en', 'intl/locale-data/jsonp/en.js'], function(require) {
+      require('intl/locale-data/jsonp/en.js');
+      const messages = require('./en');
+      cb({
+        locale: 'en',
+        messages: messages.default
+      });
+    }, 'en-intl');
+  } else {
+    require.ensure(['./en'], function(require) {
+      const messages = require('./en');
+      cb({
+        locale: 'en',
+        messages: messages.default
+      });
+    }, 'en');
+  }
+}
+
+function fr(cb) {
+  if(!hasIntl) {
+    require.ensure(['./fr', 'intl/locale-data/jsonp/fr.js', 'react-intl/locale-data/fr'], function(require) {
+      const messages = require('./fr');
+      require('intl/locale-data/jsonp/fr.js');
+      const formats = require('react-intl/locale-data/fr');
+      cb({
+        locale: 'fr',
+        messages: messages.default,
+        formats: formats
+      });
+    }, 'fr-intl');
+  } else {
+    require.ensure(['./fr', 'react-intl/locale-data/fr'], function(require) {
+      const messages = require('./fr');
+      const formats = require('react-intl/locale-data/fr');
+      cb({
+        locale: 'fr',
+        messages: messages.default,
+        formats: formats
+      });
+    }, 'fr');
+  }
+}
+
+function es(cb) {
+  if(!hasIntl) {
+    require.ensure(['./es', 'intl/locale-data/jsonp/es.js', 'react-intl/locale-data/es'], function(require) {
+      const messages = require('./es');
+      require('intl/locale-data/jsonp/es.js');
+      const formats = require('react-intl/locale-data/es');
+      cb({
+        locale: 'es',
+        messages: messages.default,
+        formats: formats
+      });
+    }, 'es-intl');
+  } else {
+    require.ensure(['./es', 'react-intl/locale-data/es'], function(require) {
+      const messages = require('./es');
+      const formats = require('react-intl/locale-data/es');
+      cb({
+        locale: 'es',
+        messages: messages.default,
+        formats: formats
+      });
+    }, 'es');
+  }
+}
+
+const locales = {
+  en: en,
+  fr: fr,
+  es: es
+};
+
+export default function(language, cb) {
+  var fn;
+  var lowercaseLanguage = language.toLowerCase();
+
+  if(locales[lowercaseLanguage]) {
+    fn = locales[lowercaseLanguage];
+  } else {
+    var shortLanguage = language.substring(0, 2);
+    fn = locales[shortLanguage] || locales.en;
+  }
+
+  var hasIntl = typeof(Intl) !== 'undefined';
+
+  if(!hasIntl) {
+    require.ensure(['intl'], function(require) {
+      require('intl');
+      fn(cb);
+    });
+  } else {
+    fn(cb);
+  }
+};
